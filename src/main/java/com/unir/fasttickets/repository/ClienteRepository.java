ee6d8633fa6b0b93e9fe02bc0347e10b73568d4f
aagomezs <adrianogomezsena@gmail.com>
2024-06-08 18:21:53 -0500

    se actualizan entidades nuevo modelo

--- src/main/java/com/unir/fasttickets/repository/ClienteRepository.java
+++ src/main/java/com/unir/fasttickets/repository/ClienteRepository.java
@@ -3,8 +3,9 @@ package com.unir.fasttickets.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.unir.fasttickets.repository.entity.ClienteEntity;
 
 @Repository
-public interface ClienteRepository extends CrudRepository<ClienteRepository,Integer>{
+public interface ClienteRepository extends CrudRepository<ClienteEntity,Integer>{
 
 }
